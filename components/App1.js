2a22d12806562091f79519ca700fea4d24b3593c
Peter <327127079@qq.com>
2022-06-17 18:07:39 +0800

    获取设备的宽高和应用

--- components/App1.js
+++ components/App1.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
+
+const {width, height,scale} = Dimensions.get('window'); //获取的这个宽度,高度不是平常的单位,是react-native的单位,同时也是可见范围内的单位
 
 const App: () => Node = () => {
   return (
-    <View style={styles.text1}>
-      <ScrollView horizontal>
-        <Text>
-          Hello WorldHello WorldHello WorldHello WorldHello WorldHello
-          WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-          WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello
-          WorldHello WorldHello WorldHello WorldHello WorldHello World
-          333
-        </Text>
-      </ScrollView>
+    <View style={styles.container}>
+      <Text>屏幕的宽度是 : {width}</Text>
+      <Text>屏幕的宽度是 : {height}</Text>
+      <Text>屏幕的像素比是 : {scale}</Text>
+      {/* 画一条一个单位的线,如果没出来,可能是虚拟机大小被调整过了,导致没显示出来 */}
+      <View style={styles.line}></View> 
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text1: {
-    width: 200,
-    height: 100,
-    backgroundColor: 'red',
-  },
-});
+    container: {
+        width,  //获取宽度,在css中设置,这样就达到了设置宽屏的效果,或者设置flex为1
+        backgroundColor:"gray",
+        height
+    },
+    line:{
+        height:1/scale,
+        backgroundColor:"red"
+    }
+})
 
 export default App;
