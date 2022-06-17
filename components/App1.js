d10e8a76bb8d4c95fcc35985ce19942074e7776a
Peter <327127079@qq.com>
2022-06-17 16:51:37 +0800

    init

---
+++ components/App1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const style
+
+export default App;
