742ffb2f7e470060572dbb3cd654450e9758edcd
Peter <327127079@qq.com>
2022-06-18 13:38:16 +0800

    flex布局: flexDirection

--- components/App1.js
+++ components/App1.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
-
-const {width, height,scale} = Dimensions.get('window'); //获取的这个宽度,高度不是平常的单位,是react-native的单位,同时也是可见范围内的单位
+import {View, Text, StyleSheet} from 'react-native';
 
 const App: () => Node = () => {
   return (
     <View style={styles.container}>
-      <Text>屏幕的宽度是 : {width}</Text>
-      <Text>屏幕的宽度是 : {height}</Text>
-      <Text>屏幕的像素比是 : {scale}</Text>
-      {/* 画一条一个单位的线,如果没出来,可能是虚拟机大小被调整过了,导致没显示出来 */}
-      <View style={styles.line}></View> 
+      <View style={styles.box1}>
+        <Text>1</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text>2</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text>3</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width,  //获取宽度,在css中设置,这样就达到了设置宽屏的效果,或者设置flex为1
-        backgroundColor:"gray",
-        height
-    },
-    line:{
-        height:1/scale,
-        backgroundColor:"red"
-    }
-})
+  container: {
+    display: 'flex',
+    /* 有四个值: row, row-reverse, column, column-reverse */
+    flexDirection: 'column-reverse',
+    flex: 1, //如果加上这条,相当于撑满屏了
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  box3: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+  },
+});
 
 export default App;
